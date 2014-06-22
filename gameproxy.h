cbac6c1936e99f55989f00027d0cc5183932fff7
Yevgen Nebesov <nebesov@gmx.de>
2014-06-22 00:41:22 -0700

    Class GameProxy is implemented

--- gameproxy.h
+++ gameproxy.h
@@ -8,8 +8,18 @@ class GameProxy
 public:
     GameProxy(Game const& game);
 
-    State getHumanState() const;
-private:
+    State getHumanState(human_t const& human, day_t const& day) const;
+
+    void setDiscovered(human_t const& human, day_t const& day);
+
+    meetings_t getMeetings(human_t const& human, day_t const& day) const;
+private: // methods
+
+    bool checkDimensions(const human_t& human, const day_t& day) const;
+    bool isDiscovered(const human_t& human, const day_t& day) const;
+private: // attributes
+
+
     Game m_game; // todo think how to assure that the server has produced the same game.
     typedef std::vector<std::vector<bool> > visibility_matrix_t;
     visibility_matrix_t visibilityMatrix;
